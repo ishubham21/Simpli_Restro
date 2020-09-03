eaf7ad47455ffa60e37d77b3957f7383cacd71fa
Shubham Gautam <sg2199203@gmail.com>
2020-09-03 22:03:32 +0530

    Cards are changed

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Function to show offers on click*/
+function myFunction() {
+  var x = document.getElementById("offer-elem");
+  if (x.style.display = "none") {
+    x.style.display = "inline-block";
+    /*Space to set animation on display*/
+  } 
+}
