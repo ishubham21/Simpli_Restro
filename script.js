fc2693b732364aa7aaafa6563cb6510ae73c8115
Shubham Gautam <sg2199203@gmail.com>
2020-09-07 18:29:41 +0530

    Collapse redefined

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/*Function to show offers on click*/
+/*Function to show offers on click
 function openOffers() {
   var x = document.getElementById("offer-elem");
   if (x.style.display = "none") {
     x.style.display = "inline-block";
-    /**/
+    
   } 
 }
